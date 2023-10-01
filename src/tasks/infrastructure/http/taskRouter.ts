db2e56e2cab875a1470ab33fb2f27f94938518c1
Carles <cpcastells@gmail.com>
2023-10-01 22:54:52 +0200

    Create getTasksController and add GET /tasks endpoint

--- src/tasks/infrastructure/http/taskRouter.ts
+++ src/tasks/infrastructure/http/taskRouter.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import createController from "./createController.js";
+import getTasksController from "./getTasksController.js";
 
 export const taskRouter = Router();
 
 taskRouter.post("/", createController);
+
+taskRouter.get("/", getTasksController);
