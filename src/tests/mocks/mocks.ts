133dd46ee07c62af04af1ce8682082adc2ae9416
Carles <cpcastells@gmail.com>
2023-10-02 00:02:14 +0200

    Create PUT /tasks/:id endpoint integration test

--- src/tests/mocks/mocks.ts
+++ src/tests/mocks/mocks.ts
@@ -6,6 +6,11 @@ export const mockedTask: Task = {
   completed: false,
 };
 
+export const updatedMockedTask: Task = {
+  description: "Hello World",
+  completed: true,
+};
+
 export const mockedTaskWithId = {
   _id: new Types.ObjectId("6478dd2fe3e61710d8baa049"),
   description: "Hello World",
