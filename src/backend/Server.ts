3191565eea5a2b2a8a993a28ef61f89ed85014ae
Carles <cpcastells@gmail.com>
2023-10-01 19:13:13 +0200

    Create Server class

---
+++ src/backend/Server.ts
@@ -0,0 +1,35 @@
+import { json } from "body-parser";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+
+export class Server {
+  private readonly express: express.Express;
+
+  constructor(private readonly port: string) {
+    this.express = express();
+    this.express.use(helmet());
+    this.express.use(cors());
+    this.express.use(json());
+  }
+
+  async listen(): Promise<void> {
+    await new Promise<void>((resolve) => {
+      this.express.listen(this.port, () => {
+        console.log(
+          `✅ Backend App is running at http://localhost:${
+            this.port
+          } in ${this.express.get("env")} mode`
+        );
+
+        console.log("✋ Press CTRL-C to stop\n");
+
+        resolve();
+      });
+    });
+  }
+
+  get app(): express.Express {
+    return this.express;
+  }
+}
