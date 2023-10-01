361deadd89f854831747d0e919df333217f2c917
Carles <cpcastells@gmail.com>
2023-10-01 23:44:47 +0200

    Update TaskRepository with update()

--- src/tasks/domain/repositories/TaskRepository.ts
+++ src/tasks/domain/repositories/TaskRepository.ts
@@ -4,4 +4,5 @@ export interface TaskRepository {
   create(task: Task): Promise<Task>;
   get(): Promise<Task[]>;
   delete(id: string): Promise<Task>;
+  update(id: string, task: Task): Promise<Task>;
 }
